1f4e9d59747ba3a6cb18f89aa1449d3fca5f4d4c
Prajwal Bati <prajwalbati@gmail.com>
2023-08-25 02:13:52 -0400

    socket message receive event and close event

--- packages/server/src/app/wsHandler.ts
+++ packages/server/src/app/wsHandler.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage } from 'http';
-import { WebSocket, WebSocketServer, ServerOptions} from 'ws';
+import { WebSocket, WebSocketServer, ServerOptions, RawData} from 'ws';
 
 export class WsHandler {
     private wsServer: WebSocketServer;
@@ -18,5 +18,25 @@ export class WsHandler {
 
     onSocketConnected(socket: WebSocket, request: IncomingMessage) {
         console.log("New websocket connection");
+
+        socket.on('message', (data: RawData) => {
+            this.onSocketMessage(socket, data);
+        });
+
+        socket.on('close', (code, reason) => {
+            this.onSocketClosed(code, reason);
+        })
     };
+
+    onSocketMessage(socket: WebSocket, data: RawData) {
+        const payload = JSON.parse(`${data}`);
+        console.log(payload);
+
+        const reply = JSON.stringify({ reply: 'Message received'});
+        socket.send(reply);
+    }
+
+    onSocketClosed(code, reason) {
+        console.log(`Client has disconnected. Code: ${code}, Reason: ${reason}`);
+    }
 }
