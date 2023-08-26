7fd035615f40fecb9fa4ae8ab6d4c7c7fdd464c1
Prajwal Bati <prajwalbati@gmail.com>
2023-08-26 14:58:35 -0400

    create user interface for message list and form

--- packages/client/src/app/app.component.ts
+++ packages/client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatRelayMessage, User } from '@chat-with-socket/types';
 
 @Component({
   selector: 'chat-with-socket-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'I am angular';
+
+  messages: ChatRelayMessage[] = [];
+  currentUser: User;
+
+  ngOnInit(): void {
+      this.messages = [
+        { event: 'chatRelay', author: {name: 'Jane', id: 1}, contents: 'Hi this is Jane'},
+        { event: 'chatRelay', author: {name: 'Henry', id: 1}, contents: 'Hi this is Henry'}
+      ];
+
+      // this.currentUser = {};
+  }
+
+  connect(userNameInput: HTMLInputElement) {
+    console.log("connect");
+    console.log(userNameInput);
+  }
 }
--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatInputModule, MatButtonModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
